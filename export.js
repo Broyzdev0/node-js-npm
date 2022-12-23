a6e1839ccce59687041800c62556b4eb2b913325
Broyzdev0 <broyzdesign240904@gmail.com>
2022-12-23 14:13:39 +0700

    updatez

---
+++ export.js
@@ -0,0 +1,3 @@
+import { writeToFile } from "node-js-npm/write";
+
+writeToFile("export.log", "Belajar exports");
---
+++ lodash.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+
+const name = "ANGGA NUR GUNAWAN";
+const result = _.capitalize(name);
+
+console.info(result);
